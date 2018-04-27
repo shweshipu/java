10e2bdf3aa81c7e6e9b785ba98b0037b51072668
Steven Harper <shwesh279@gmail.com>
2018-04-27 13:56:54 -0700

    added through command line

--- equations/JavaPlot3d.java
+++ equations/JavaPlot3d.java
@@ -1,3 +1,8 @@
+// use java JavaPlot3d 1 0 0
+//if it doesnt draw blue lines at 0,0 and u get alot of yellow just re run
+
+
+
 //code by CW Coleman
 //save as JavaPlot.java
 import java.awt.*;
@@ -9,73 +14,72 @@ public class JavaPlot3d extends JFrame {
    public JavaPlot3d()
    {
       super( "Java Plot" );
-      setSize(400,430);
+      setSize(dimx,dimy + 30);
       setVisible( true );
    }
    public void paint( Graphics g ){
-        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		    // only change code below this line
+        //clear stuff. (unnecessary?)
         int x, y, h, k, t;
         int iRed, iGreen, iBlue, aiRed, aiGreen, aiBlue;//integer RGB
         x = 0;  y = 0; h = 0; k = 0; t = 0;
 
         //blank start
-        for (x = 0;x <400;x++){
-          for (y = 0; y < 400;y++){
-              iRed = 155; iGreen = 155; iBlue = 155;
+        for (x = 0;x <dimx;x++){
+          for (y = 0; y < dimy + 30;y++){
+              iRed = 255; iGreen = 255; iBlue = 255;
               g.setColor(new Color(iRed,iGreen,iBlue));
               g.drawLine(x,y,x,y);
 
           }
 
-      }//end for
-
-
-      /*
-      iRed = 255; iGreen = 255; iBlue = 255;
-        g.setColor(new Color(iRed,iGreen,iBlue));
-        h = -200;
-        k = 0;
-      for (x = 0;x < 400;x++){
-        k =  3*h -10;
-        h++;
-
-        g.drawLine(h+200,200-k,h+200,200-k);
-        System.out.println(h+"<-x k->"+k);
       }
-      */
-
       //Graph 3d
-
-      for (x = 0;x <400;x++){
-        for (y = 0; y < 400;y++){
+      for (x = 0 + posx;x <dimx + posx;x++){
+        for (y = 0 + posy; y < dimy + posy;y++){
           iRed = 0; iGreen = 0; iBlue = 0;
+          rgb = hex(funct(x,y));
           //get the z value
-          iGreen = funct(x,y);
+          iGreen = ((int)funct(x,y));
           //fix bounds with other colors
           if (iGreen>255){
-            iRed = funct(x,y)%255;
-            iGreen = funct(x,y)%255;
+            iRed = ((int)funct(x,y))%256;
+            iGreen = ((int)funct(x,y))%256;
           }
           if (iGreen<0){
-            iRed =255 + funct(x,y)%255;
-            iBlue = 255 + funct(x,y)%255;
+            iRed =255 + ((int)funct(x,y))%256;
+            iBlue = 255 + ((int)funct(x,y))%256;
             iGreen = 0;
           }
-          g.setColor(new Color(iRed,iGreen,iBlue));
-          g.drawLine(x,y+30,x,y+30);
+          if(funct(x,y) == 0){//color zero
+            iRed = 255; iGreen = 255; iBlue = 255;
+          }
+          if(funct(x,y) == 1){//color one
+            iRed = 127; iGreen = 127; iBlue = 127;
+          }
+          //g.setColor(new Color(iRed,iGreen,iBlue));
+          g.setColor(new Color(rgb[0],rgb[1],rgb[2]));
+          g.drawLine(x-posx,y+30-posy,x-posx,y+30-posy);
 
         }
       }
     //make axis
+     posx += dimx/2;
+     posy += dimy/2;
     iRed = 0; iGreen = 0; iBlue = 255;
     g.setColor(new Color(iRed,iGreen,iBlue));
-    g.drawLine(0,200,400,200);
-    g.drawLine(200,0,200,400);
+    if(dimy/2 + 30 - posy>=0 && dimy/2 + 30 - posy <= dimy ){
+      g.drawLine(0,dimy/2 +30 - posy,dimx,dimy/2 +30 -posy);
+    }
+    if(dimx/2 - posx>=0 && dimx/2 - posx<=dimy){
+      g.drawLine(dimx/2  - posx,30,dimx/2 - posx,dimy + 30);
+    }
 
   }
-  public int funct(int x,int y){
-    return(
+  public double funct(int x,int y){
+    double dubx=((double)x)*scale;
+    double duby=((double)y)*scale;
+
+    return((
       /*
       (int)  Math.sqrt(
               (double)(y*y + x*x)
@@ -83,13 +87,52 @@ public class JavaPlot3d extends JFrame {
               *2
       *255/400
       */
-      x*y
+      Math.pow(dubx,duby)
+      //x*y
 
-    );
+    ));
+  }
+  public int[] hex(double z){
+    int rgbtmp[] = new int[3];
+    if(z<0){
+      z = 256*256*256-z;
+    }
+    int iz = (int) z;
+
+    int b = iz%256;
+    rgbtmp[2] = b;
+    iz = iz-b;
+    System.out.println(iz);
+    if(iz>0){
+      int g = iz%(256*256);
+      rgbtmp[1] = g;
+      iz = iz-g;
+    }
+    if(iz>0){
+      int r = iz%(256*256*256);
+      rgbtmp[0] = r;
+      iz = iz-r;
+    }
+
+    if(rgbtmp[0]>=256){
+      rgbtmp[0] = 255;
+    }
+    System.out.println(z + " " +rgbtmp[0] + " " + rgbtmp[1] + " "+  rgbtmp[2]);
+    return(rgbtmp);
   }
 
 // execute application
-  public static void main( String args[] ){
+static int rgb[] = new int[3];
+static double scale = 1;
+static int posx = 0;
+static int posy = 0;
+static int dimx = 400;
+static int dimy = 400;
+  public static void main( String a[] ){
+
+    scale = Double.parseDouble(a[0]); //bigger for 'zoom out'
+    posx = Integer.parseInt(a[1]) -dimx/2;//goes right
+    posy = Integer.parseInt(a[2]) -dimy/2;//goes down
     JavaPlot3d myobject = new JavaPlot3d();//change this
   	   	// adapter to handle only windowClosing event
         myobject.addWindowListener(
